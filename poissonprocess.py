bc65359a8b2052b77170e5352ea11280f6a1fb40
sschube6 <sschube6@gmail.com>
2015-11-14 23:21:28 +0000

    Initial commit of poissonprocess script

---
+++ poissonprocess.py
@@ -0,0 +1,46 @@
+'''
+Poisson Process
+This scripts simulates a poisson process. Individual events are arriving with an average rate k within the time [tstart, tend]. The distribution as well as a histogram of the waiting times dt is plotted.
+'''
+
+import numpy as np
+from matplotlib import pyplot
+
+tstart = 0
+tend = 100
+k = 1
+
+u = np.random.rand(int((tend-tstart)*k*10))
+while(True):
+    iszero = (u == 0)
+    if iszero.any():
+        u[iszero] = np.random.rand(sum(iszero))
+        print sum(iszero)
+    else:
+        break
+dt = -(1.0/k)*np.log(u)
+t = tstart+np.cumsum(dt)
+t = t[t < tend]
+
+'''plotting'''
+fig = pyplot.figure()
+
+fig.add_subplot(211,
+                title='Arrival times\n(at average rate k = %g per s)' % k,
+                xlabel='time t in s')
+pyplot.hlines(1, tstart, tend, colors='k')
+pyplot.plot(t, np.ones(t.shape),
+            linestyle='none',
+            marker='|',
+            markerfacecolor='none',
+            markersize=100)
+pyplot.ylim(0.9, 1.1)
+pyplot.yticks([])
+
+fig.add_subplot(212,
+                title='Distribution of interval times',
+                xlabel='interval time dt in s',
+                ylabel='occurences')
+pyplot.hist(dt[0:t.size])
+fig.tight_layout()
+pyplot.show()
